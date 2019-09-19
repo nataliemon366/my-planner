a0c30b99448d219b2dd6cc536c66d52c72a24925
jerniceduncangit config --global user.name git config --global user.name :jerniceduncan <jerniceduncan@icloud.com>
2019-09-19 14:34:05 -0700

    added randomquotes

--- assets/js/duncan.js
+++ assets/js/duncan.js
@@ -12,15 +12,6 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
-  db.collection("toAdd").add({
-   
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
 
 // function render() {
 //   var form = $("<form id='todo-form'>");
@@ -28,39 +19,83 @@ var db = firebase.firestore();
 //   var submit = $("<input type='submit' value='Add To Do'>");
 //   var item = $("<todo-list-item>");
 //   form.append(todoInput, submit, item);
-//   $("body").prepend(form);
+
+//   var ul = $("<ul id='todo-list'>");
+
+//   $("body").prepend(form, ul);
 // }
 // render();
-// $(document)
-//   .on("submit", "#todo-form", function() {
-//     event.preventDefault();
-//     $("#todo").val();
-  
+
+// $(document).on("submit", "#todo-form", function() {
+//   event.preventDefault();
+//   var todo = $("#todo").val();
+//   console.log(todo);
+
+//   // save it
+//   db.collection("Todo")
+//     .add({ todo: todo })
+//     .then(function(docRef) {
+//       console.log("Document written with ID: ", docRef.id);
+
+
+//       var btnDone = $("<button>").text("Done")
+//       var btnRemove = $("<button>").text("Remove")
+
+//       // show it
+//       var li = $("<li>").text(todo).attr("data-id", docRef.id);
+//       li.append(btnDone, btnRemove)
+//       $("#todo-list").append(li);
+//     })
+//     .catch(function(error) {
+//       console.error("Error adding document: ", error);
+//     });
+// });
+
+$(document).ready(function() {
+  $("#button").click(function() {
+    var toAdd = $("input[name=ListItem]").val();
+    $("ol").append("<li>" + toAdd + "</li>");
+  });
+
+  $("input[name=ListItem]").keyup(function(event) {
+    if (event.keyCode == 13) {
+      $("#button").click();
+    }
+  });
+
+  $(document).on("dblclick", "li", function() {
+    $(this)
+      .toggleClass("strike")
+      .fadeOut("slow");
+  });
+
+  $("input").focus(function() {
+    $(this).val("");
+  });
+
+
+
+  var quotes =
+  [
+    "It does not matter how slowly you go as long as you do not stop.",
+    "Only I can change my life. No one can do it for me.",
+    "Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.",
+    "Good, better, best. Never let it rest. 'Til your good is better and your better is best.",
+    "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+    "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+    "We must let go of the life we have planned, so as to accept the one that is waiting for us.",
+    "The privilege of a lifetime is being who you are."
+  ];
+ 
 
   
-$(document).ready(
-  function(){
-      $('#button').click(
-          function(){
-              var toAdd = $('input[name=ListItem]').val();
-               $('ol').append('<li>' + toAdd + '</li>');
-          });
-     
-     $("input[name=ListItem]").keyup(function(event){
-        if(event.keyCode == 13){
-          $("#button").click();
-        }         
-    });
-    
-    $(document).on('dblclick','li', function(){
-      $(this).toggleClass('strike').fadeOut('slow');    
-    });
-    
-    $('input').focus(function() {
-      $(this).val('');
-    });
-    
-     
-    
-  }
-);
+  function randomQuotes(arr){
+    return arr[(Math.floor(Math.random() * arr.length))];
+  } 
+  $("#quotes").text( randomQuotes(quotes))
+ 
+
+
+
+
+});
